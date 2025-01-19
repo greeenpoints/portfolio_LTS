1f73935a57353736da0f3a784f2f00dcaa09f673
Prince_Afdob <abhishek98.social@gmail.com>
2025-01-19 15:23:19 +0530

    DL

--- pages/apple.js
+++ pages/apple.js
@@ -1,9 +1,6 @@
-import Head from 'next/head';
-import Image from 'next/image';
+
 import React, { useEffect, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import TextLink from '../components/TextLink';
-import Link from 'next/link';
+import { motion} from 'framer-motion';
 import GridContainer from '../components/GridContainer';
 import ProjectHero from '../components/projects/ProjectHero';
 import ProjectBody from '../components/projects/ProjectBody';
