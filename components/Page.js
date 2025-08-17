aef6daffb23792ccdc6aff471fe93ac8f33214d4
Abhishek <abhishek98.social@gmail.com>
2025-08-17 15:42:48 +0530

    Update color scheme to use black and yellow accents
    Replaced #111111 backgrounds with black and updated accent colors from blue (#1E4DD9) to yellow (#e9dd34, yellow-300) in both Page.js and globals.scss. This unifies the color palette and refreshes the visual style.

--- components/Page.js
+++ components/Page.js
@@ -6,7 +6,7 @@ export default function Page(props) {
   return (
     <>
       <div className="w-full flex items-center flex-col z-[200]">
-        <div className="z-10 w-full h-full bg-[#111111] justify-center flex">
+  <div className="z-10 w-full h-full bg-black justify-center flex">
           <div
             className={`flex relative flex-col px-3 pb-8 w-full border-b border-solid shadow-xl max-w-8xl z-[9] text-padding md:px-3 border-neutral-800`}
           >
