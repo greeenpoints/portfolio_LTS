d4758761cb928f1d4caf1c86b0b599c873ea7adf
Abhishek <abhishek98.social@gmail.com>
2025-10-23 19:10:30 +0530

    Add Google Analytics pageview tracking on route change
    Introduced a useEffect hook in _app.js to send pageview events to Google Analytics whenever the route changes, using Next.js router events.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
 import '../styles/globals.scss';
 import Page from '../components/Page';
 import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      // Send pageview to Google Analytics
+      window.gtag('config', 'G-YSX5R91XDE', {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Page>
       <Component {...pageProps} />
