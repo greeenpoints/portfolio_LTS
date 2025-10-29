52e258ec47c2f540a00ad424840c959276e7e384
Abhishek <abhishek98.social@gmail.com>
2025-10-29 08:09:11 +0530

    Move ERSS SVGs to new directory and update imports
    Renamed all ERSS SVG files from public/ERSS/ to public/work/erss/ for better organization. Updated image source paths in pages/erss.js to reflect the new location.

--- pages/erss.js
+++ pages/erss.js
@@ -12,7 +12,7 @@ export default function ERSS() {
         <div className="col-span-full md:col-span-9 md:col-start-4 space-y-0">
           <div className="space-y-0">
             <Image
-              src="/erss/erss1.svg"
+              src="/work/erss/erss1.svg"
               alt="Emergency Response Support System interface concept"
               width={1280}
               height={720}
@@ -20,112 +20,112 @@ export default function ERSS() {
               priority
             />
             <Image
-              src="/ERSS/erss2.svg"
+              src="/work/erss/erss2.svg"
               alt="Emergency Response Support System journey visualization"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss3.svg"
+              src="/work/erss/erss3.svg"
               alt="Emergency Response Support System interface detail 3"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss4.svg"
+              src="/work/erss/erss4.svg"
               alt="Emergency Response Support System interface detail 4"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss5.svg"
+              src="/work/erss/erss5.svg"
               alt="Emergency Response Support System interface detail 5"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss6.svg"
+              src="/work/erss/erss6.svg"
               alt="Emergency Response Support System interface detail 6"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss7.svg"
+              src="/work/erss/erss7.svg"
               alt="Emergency Response Support System interface detail 7"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss8.svg"
+              src="/work/erss/erss8.svg"
               alt="Emergency Response Support System interface detail 8"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss9.svg"
+              src="/work/erss/erss9.svg"
               alt="Emergency Response Support System interface detail 9"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss10.svg"
+              src="/work/erss/erss10.svg"
               alt="Emergency Response Support System interface detail 10"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss11.svg"
+              src="/work/erss/erss11.svg"
               alt="Emergency Response Support System interface detail 11"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss12.svg"
+              src="/work/erss/erss12.svg"
               alt="Emergency Response Support System interface detail 12"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss13.svg"
+              src="/work/erss/erss13.svg"
               alt="Emergency Response Support System interface detail 13"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss14.svg"
+              src="/work/erss/erss14.svg"
               alt="Emergency Response Support System interface detail 14"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss15.svg"
+              src="/work/erss/erss15.svg"
               alt="Emergency Response Support System interface detail 15"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss16.svg"
+              src="/work/erss/erss16.svg"
               alt="Emergency Response Support System interface detail 16"
               width={1280}
               height={720}
               className="block w-full h-auto"
             />
             <Image
-              src="/ERSS/erss17.svg"
+              src="/work/erss/erss17.svg"
               alt="Emergency Response Support System interface detail 17"
               width={1280}
               height={720}
